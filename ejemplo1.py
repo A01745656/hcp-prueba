5a274b2764f7d5602d25a4fa7c385c0e35307730
A01745656 <a01745656@itesm.mx>
2020-10-29 13:04:04 -0600

    agregar nuevos archivos

--- ejemplo1.py
+++ ejemplo1.py
@@ -7,7 +7,12 @@ print(imagen[0][0][0])
 imagen = cv2.resize(imagen,(256,256))
 
 cv2.imwrite("resizeimagen.jpg",imagen)
-cv2.imshow("image",imagen)
-cv2.waitKey(0)
+imagen = cv2.imread("imagen.jpg")
+imagen = cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+print(imagen.shape)
+print(imagen[0][0])
+cv2.imwrite("grayimagen.jpg",imagen)
+
+
 
 
